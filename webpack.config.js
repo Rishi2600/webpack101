e13b5820d5339e689bca8d8a25536337e2ebb672
Rishi2600 <rishiraj6177@gmail.com>
2025-03-08 00:13:52 +0530

    plugin config done

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: "./src/index.js",
@@ -21,5 +22,6 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-  }
+  },
+  plugins: [new HtmlWebpackPlugin()],
 };
