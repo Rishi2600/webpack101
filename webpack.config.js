1922ffde752221cecec80defe0f41ab8e8597d1b
Rishi2600 <rishiraj6177@gmail.com>
2025-03-07 23:57:25 +0530

    babel config

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,19 @@
 module.exports = {
-    entry: "./src/index.js",
-    module: {
-        rules: [
-            {
-                test: /\.svg$/,
-                use: 'svg-inline-loader'
-            },
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"],
-              }
-        ]
-    }
-}
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        use: "svg-inline-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(js)$/,
+        use: "babel-loader"
+      }
+    ],
+  },
+};
